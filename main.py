a5d8372c18b44a8a327dfca18dba5e18850e2e90
Vikseko <vikseko@gmail.com>
2021-11-17 14:37:02 +0800

    Version 1.0

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from Builder import *
 from Parser import *
 from Pathfinder import *
-# Press Shift+F10 to execute it.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     start_time = time.time()
     parser = createParser()
