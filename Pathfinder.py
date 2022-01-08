9e20d02ae015b3592d1b528d4769fda91005d997
Vikseko <vikseko@gmail.com>
2022-01-08 21:00:12 +0800

    Version 1.1 Redirection v3

--- Pathfinder.py
+++ Pathfinder.py
@@ -1,6 +1,6 @@
 from Utils import *
 from Builder import DisjunctiveDiagramsBuilder
-#from Redirection import *
+from Redirection import *
 
 # Получаем КНФ из диаграммы (все пути из корней в терминальную 'true')
 def GetCNFFromDiagram(diagram:DisjunctiveDiagram):
@@ -82,6 +82,46 @@ def WritePaths(problem,node_paths,node_path,clause):
             lnode_path.append(node)
             WritePaths(problem, node_paths, lnode_path, lclause)
 
+
+# Считаем question paths в диаграмме (все пути из корней в терминальную '?')
+def CountQuestionPathsInDiagram(diagram:DisjunctiveDiagram):
+    paths_table = set()
+    question_leaf = diagram.GetQuestionLeaf()
+    for node in question_leaf.high_parents:
+        clause = []
+        clause.append(node.var_id)
+        node_path = []
+        node_path.append(node)
+        CountPaths(node_path,clause,paths_table)
+    for node in question_leaf.low_parents:
+        clause = []
+        clause.append(-node.var_id)
+        node_path = []
+        node_path.append(node)
+        CountPaths(node_path,clause,paths_table)
+    return len(paths_table)
+
+
+def CountPaths(node_path,clause,paths_table):
+    current_node:DiagramNode = node_path[-1]
+    if current_node.IsRoot():
+        clause.reverse()
+        clause.sort()
+        paths_table.add(tuple(clause))
+    else:
+        for node in current_node.high_parents:
+            hclause = copy.copy(clause)
+            hnode_path = copy.copy(node_path)
+            hclause.append(node.var_id)
+            hnode_path.append(node)
+            CountPaths(hnode_path, hclause,paths_table)
+        for node in current_node.low_parents:
+            lclause = copy.copy(clause)
+            lnode_path = copy.copy(node_path)
+            lclause.append(-node.var_id)
+            lnode_path.append(node)
+            CountPaths(lnode_path, lclause,paths_table)
+
 # Получаем false paths из диаграммы (все пути из корней в терминальную '?')
 """идём по графу снизу вверх, как только дошли до корня, пометили как посещенный, 
 в следующий раз дойдём до другого корня.
@@ -140,7 +180,6 @@ def RedirPaths(node_path,clause, g, diagram, finded_flag):
         #print(clause)
         s, model = PathCheck(clause, g)
         if s == False:
-            pass
             flag_copy_path = RedirectPath(clause, node_path, diagram)
             if flag_copy_path == True:
                 diagram.copy_redir += 1
@@ -183,3 +222,117 @@ def RedirPaths(node_path,clause, g, diagram, finded_flag):
                     finded_flag = RedirPaths(lnode_path, lclause, g, diagram, finded_flag)
                     if finded_flag:
                         return True
+
+
+def RedirectQuestionPathsFromDiagram_v3(diagram:DisjunctiveDiagram, question_paths_count):
+    cnf, tmp_ = GetCNFFromDiagram(diagram)
+    cnf = CNF(from_clauses=cnf)
+    g = MapleChrono(bootstrap_with=cnf)
+    #g.conf_budget(1000)
+    question_leaf = diagram.GetQuestionLeaf()
+    clauses_set = set()
+    while not question_leaf.IsVisited():
+        #print('\nSearch in progress')
+        finded_flag = False
+        if diagram.question_path_count_ % 2 == 0:
+            for node in question_leaf.high_parents:
+                if not finded_flag:
+                    clause = []
+                    clause.append(node.var_id)
+                    node_path = []
+                    node_path.append(node)
+                    finded_flag = RedirPaths_v3(node_path,clause, g, diagram, finded_flag,clauses_set, question_paths_count)
+                    if finded_flag:
+                        break
+            if finded_flag == True:
+                continue
+            for node in question_leaf.low_parents:
+                if not finded_flag:
+                    clause = []
+                    clause.append(-node.var_id)
+                    node_path = []
+                    node_path.append(node)
+                    finded_flag = RedirPaths_v3(node_path,clause, g, diagram, finded_flag,clauses_set, question_paths_count)
+                    if finded_flag:
+                        break
+        else:
+            for node in reversed(question_leaf.low_parents):
+                if not finded_flag:
+                    clause = []
+                    clause.append(-node.var_id)
+                    node_path = []
+                    node_path.append(node)
+                    finded_flag = RedirPaths_v3(node_path,clause, g, diagram, finded_flag,clauses_set, question_paths_count)
+                    if finded_flag:
+                        break
+            if finded_flag == True:
+                continue
+            for node in reversed(question_leaf.high_parents):
+                if not finded_flag:
+                    clause = []
+                    clause.append(node.var_id)
+                    node_path = []
+                    node_path.append(node)
+                    finded_flag = RedirPaths_v3(node_path,clause, g, diagram, finded_flag,clauses_set, question_paths_count)
+                    if finded_flag:
+                        break
+        if finded_flag == False:
+            question_leaf.Visit()
+    #print('Number of question paths:'.ljust(30, ' '), diagram.question_path_count_)
+    print('Number of solved paths'.ljust(30, ' '),  diagram.copy_redir + diagram.uniq_redir)
+    print('Number of copy redirected'.ljust(30, ' '), diagram.copy_redir)
+    print('Number of uniq redirected'.ljust(30, ' '), diagram.uniq_redir)
+
+
+def RedirPaths_v3(node_path,clause, g, diagram, finded_flag,clauses_set, question_paths_count):
+    current_node:DiagramNode = node_path[-1]
+    if current_node.IsRoot():
+        clause.reverse()
+        if tuple(sorted(clause)) not in clauses_set:
+            clauses_set.add(tuple(sorted(clause)))
+            node_path.reverse()
+            diagram.question_path_count_ += 1
+            #sys.stdout.write("Checking path %s of %s\r" % (diagram.question_path_count_,question_paths_count))
+            #sys.stdout.flush()
+            #print('Find new path: ',clause)
+            s, model = PathCheck(clause, g)
+            if s == False:
+                #print('SAT-oracle says False. Redirect path.')
+                flag_copy_path = RedirectPath(clause, node_path, diagram)
+                if flag_copy_path == True:
+                    diagram.copy_redir += 1
+                else:
+                    diagram.uniq_redir += 1
+            if s == None:
+                #print('SAT-oracle says None. Go next path.')
+                return False
+            if s == True:
+                #print('Problem solved due false path checking.')
+                #print('Model:', model)
+                # break
+                pass
+            #current_node.Visit()
+            return True
+        else:
+            #print('Finded path is already in table, go next: ', clause)
+            return False
+    else:
+        for node in current_node.high_parents:
+            if not finded_flag:
+                hclause = copy.copy(clause)
+                hnode_path = copy.copy(node_path)
+                hclause.append(node.var_id)
+                hnode_path.append(node)
+                finded_flag = RedirPaths_v3(hnode_path, hclause, g, diagram, finded_flag,clauses_set, question_paths_count)
+                if finded_flag:
+                    return True
+        for node in current_node.low_parents:
+            if not finded_flag:
+                lclause = copy.copy(clause)
+                lnode_path = copy.copy(node_path)
+                lclause.append(-node.var_id)
+                lnode_path.append(node)
+                finded_flag = RedirPaths_v3(lnode_path, lclause, g, diagram, finded_flag,clauses_set, question_paths_count)
+                if finded_flag:
+                    return True
+        return False
